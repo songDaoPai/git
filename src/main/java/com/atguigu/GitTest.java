ac0eef7553db84bd2ca5f9ab607559453871c795
songDaoPai <73736814+songDaoPai@users.noreply.github.com>
2022-01-02 22:12:23 +0800

    master pull

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master test~");
         System.out.println("hot-fix test~");
         System.out.println("master push");
+         System.out.println("master pull");
     }
 }
