acd8106b02b0ba1403ace719b5119d7bc7922a7c
ZeroBroken <ZeroBroen@qq.com>
2022-01-02 16:52:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author: 零  碎
+ * @date: 2022-01-02 16:32
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git~");
+    }
+}
